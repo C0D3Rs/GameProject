ebb832f494517b894018bdd533216e3b03586785
Miłosz Osiński <mylosz@yahoo.pl>
2013-11-04 15:20:25 +0100

    dodanie modelu i controllera image

---
+++ GameProject/Models/Entities/Image.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GameProject.Models.Entities
+{
+    public class Image
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public byte[] Data { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Timestamp]
+        public byte[] CurrentVersion { get; set; }
+    }
+}

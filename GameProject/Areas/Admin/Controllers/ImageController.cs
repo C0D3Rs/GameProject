09c1b5644a12ef2aac81631e0509bdcb8f0b8a8e
Miłosz Osiński <mylosz@yahoo.pl>
2013-11-11 15:18:23 +0100

    add validates in Create and show find by imageName

--- GameProject/Areas/Admin/Controllers/ImageController.cs
+++ GameProject/Areas/Admin/Controllers/ImageController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -29,40 +30,68 @@ namespace GameProject.Areas.Admin.Controllers
         [HttpPost]
         public ActionResult Create(HttpPostedFileBase file,ImageCategory category)
         {
-            if (file != null && file.ContentLength > 0)
+            //jeśli nie jest pusty i mniejszy niż 200kb
+            if (file != null && file.ContentLength > 0 && file.ContentLength < 204800)
             {
                 // Get file info
                 var fileName = Path.GetFileName(file.FileName); //nazwa pliku
+                var imageName = Path.GetFileNameWithoutExtension(fileName);
+                var imageExtension = "";
                 var contentLength = file.ContentLength; //wielkosc pliku
                 var contentType = file.ContentType; //typ pliku
+                
+                if (contentType == "image/jpeg") { imageExtension = "jpg";}
+                else if (contentType == "image/png") { imageExtension = "png";}
 
-                byte[] imageBytes = new byte[contentLength - 1];
-                using (var binaryReader = new BinaryReader(file.InputStream))
+                if (imageExtension != "")
                 {
-                    imageBytes = binaryReader.ReadBytes(file.ContentLength);
-                }
-                Image img = new Image();
-                img.FileName = fileName;
-                img.Data = imageBytes;
-                img.Type = contentType;
-                img.Category = category;
-                db.Images.Add(img);
-                db.SaveChanges();
+                    byte[] imageBytes = new byte[contentLength - 1];
+                    using (var binaryReader = new BinaryReader(file.InputStream))
+                    {
+                        imageBytes = binaryReader.ReadBytes(file.ContentLength);
+                    }
+                    Image img = new Image();
+                    img.FileName = String.Format("{0:yyyyMMddHHmmss}.{1}", DateTime.Now, imageExtension);
+                    img.Data = imageBytes;
+                    img.Type = contentType;
+                    img.Category = category;
+                    db.Images.Add(img);
+                    db.SaveChanges();
 
-                // redirect back to the index action to show the form once again
-                FlashMessageHelper.SetMessage(this, FlashMessageType.Success, "Dodanie zdjęcia przebiegło pomyślnie.");
-                return RedirectToAction("Index");
+                    // redirect back to the index action to show the form once again
+                    FlashMessageHelper.SetMessage(this, FlashMessageType.Success, "Dodanie zdjęcia przebiegło pomyślnie.");
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    FlashMessageHelper.SetMessage(this, FlashMessageType.Danger, "Uwaga niewłaściwy format zdjęcia. Akceptowane formaty: jpg, png."); 
+                    return RedirectToAction("Create");
+                }
             }
             else
             {
                 // redirect back to the index action to show the form once again
+                FlashMessageHelper.SetMessage(this, FlashMessageType.Danger, "Uwaga nie wybrano pliku lub jego wielkość przekracza 200kb.");
                 return RedirectToAction("Create");
             }
         }
 
-        public ActionResult Show()
+        public ActionResult Show(string imageName = "")
         {
-            var img = (from x in db.Images orderby x.ID descending select x).FirstOrDefault();
+            if (imageName == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var query = from a in db.Images
+                        where a.FileName.Equals(imageName)
+                        select a;
+
+            var img = query.FirstOrDefault();
+
+            if (img == null)
+            {
+                return HttpNotFound();
+            }
 
             return File(img.Data, img.Type);
         }

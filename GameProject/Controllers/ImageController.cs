00a398e19873335697e6c0d27451c764e5560ddf
Miłosz Osiński <mylosz@yahoo.pl>
2013-11-20 23:01:33 +0100

    add Show image in controlers and add link in index images

---
+++ GameProject/Controllers/ImageController.cs
@@ -0,0 +1,43 @@
+﻿using GameProject.Enums;
+using GameProject.Filters;
+using GameProject.Helpers;
+using GameProject.Models;
+using GameProject.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameProject.Controllers
+{
+    public class ImageController : Controller
+    {
+
+        private DatabaseContext db = new DatabaseContext();
+
+        public ActionResult Show(string imageName = "")
+        {
+            if (String.IsNullOrEmpty(imageName))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var query = from a in db.Images
+                        where a.FileName == imageName
+                        select a;
+
+            var img = query.FirstOrDefault();
+
+            if (img == null)
+            {
+                return HttpNotFound();
+            }
+
+            return File(img.Data, img.Type);
+        }
+    }
+}

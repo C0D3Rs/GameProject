4c05c3bef96f434cf12c7670d415a1c3e817446f
Lukasz Paczynski <luki00765@gmail.com>
2014-01-06 00:46:58 +0100

    Messages v1 , update comming soon

---
+++ GameProject/Enums/MessageType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameProject.Enums
+{
+    public class MessageType
+    {
+        public enum MessageOfType
+        {
+            messageFromSystem = 1,
+            messageFromUser = 2,
+        }
+    }
+}
---
+++ GameProject/Models/Entities/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GameProject.Models.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string FromUser { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
+        public DateTime Date { get; set; }
+        [DataType(DataType.MultilineText)]
+        [Required]
+        public string ContentOfMessage { get; set; }
+        [Required]
+        public int ToUserId { get; set; }
+    }
+}
